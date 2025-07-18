3f8a845644f971f79029705b7ae487f9cd4de880
Rifky Akhmad F <38658854+RifkyA911@users.noreply.github.com>
2025-07-18 14:33:04 +0700

    feat: add UserController routes and enhance image retrieval functionality

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,4 +19,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('posts', PostController::class);
     Route::apiResource('posts.likes', LikeController::class);
     Route::apiResource('posts.comments', CommentController::class);
+
+    Route::apiResource('users', UserController::class);
 });
+
+
+Route::get('/image/{path}', function ($path) {
+    $filePath = storage_path('app/public/' . $path);
+
+    if (!file_exists($filePath)) {
+        abort(404);
+    }
+
+    return response()->file($filePath);
+})->where('path', '.*');
+
